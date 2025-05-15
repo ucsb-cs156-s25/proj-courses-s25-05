02b39e32a28a207af015c03191a6550f42529acf
Austin Quach <austinquach20@gmail.com>
2025-05-14 18:45:39 -0700

    removed commented code

--- frontend/src/stories/pages/GeneralEducation/GESearchIndexPage.stories.js
+++ frontend/src/stories/pages/GeneralEducation/GESearchIndexPage.stories.js
@@ -1,11 +1,8 @@
 import React from "react";
 import GESearchIndexPage from "main/pages/GeneralEducation/GESearchIndexPage";
 
-// import { geSearchFixtures } from "fixtures/geSearchFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
-// import { oneSection } from "fixtures/sectionFixtures";
 
-// import { toast } from "react-toastify";
 import { http, HttpResponse } from "msw";
 
 export default {
@@ -18,11 +15,6 @@ const Template = () => <GESearchIndexPage />;
 export const Default = Template.bind({});
 Default.parameters = {
   msw: [
-    // http.get("/api/UCSBSubjects/all", () => {
-    //   return HttpResponse.json(ucsbSubjectsFixtures.threeSubjects, {
-    //     status: 200,
-    //   });
-    // }),
     http.get("/api/systemInfo", () => {
       return HttpResponse.json(systemInfoFixtures.showingBoth, {
         status: 200,
