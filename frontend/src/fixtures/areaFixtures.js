5e38a0546633e5200e3cbbcd2902187b8fe0b41e
Austin Quach <austinquach20@gmail.com>
2025-05-15 14:28:57 -0700

    form working on local storybook

---
+++ frontend/src/fixtures/areaFixtures.js
@@ -0,0 +1,9 @@
+export const allTheAreas = [
+    ["A", "English Reading and Composition"],
+    ["B", "Foreign Language"],
+    ["C", "Science, Mathematics, and Technology"],
+    ["D", "Social Sciences"],
+    ["E", "Culture and Thought"],
+    ["F", "Arts"],
+    ["G", "Literature"],
+];
